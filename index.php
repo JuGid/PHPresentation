f8e66836da1773f1038559979f2f433f05daecda
Julien Gidel <gidjulien@gmail.com>
2020-03-04 10:32:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,26 +5,26 @@ use PHPresentation\PHPresentation;
 use PHPresentation\Utils\Paginator;
 
 $presentation = new PHPresentation();
-$presentation->name('Présentation de test');
-$presentation->author('Julien GIDEL');
+$presentation->name('My pretty presentation');
+$presentation->author('User Name');
 $presentation->version(0,0,1);
 
-$presentation->createSection("Première section de test")
+$presentation->createSection("First section")
              ->createSlide()
-             ->title('Ceci est un titre')
-             ->text('Bienvenue sur la page test')
-             ->text('Et voici un nouveau text');
+             ->title('This is a text')
+             ->text('This is another text')
+             ->text('And this is too !');
 $presentation->last()
              ->createSlide()
-             ->title('Test de liste')
-             ->list(['Nouvelle', 'Liste', 'Element']);
-$presentation->createSection("Seconde section")
+             ->title('You can create lists')
+             ->list(['Element 1', 'Element 2', 'Element 3']);
+$presentation->createSection("Second section")
              ->createSlide()
-             ->title('Un second titre')
+             ->title('This is a title')
              ->block('Block titre 1', 'Block content 1')
              ->block('Block titre 2', 'Block content 2')
              ->block('Block titre 3', 'Block content 3')
-             ->text('Et voici un nouveau text component');
+             ->text('And this is a text component');
 
 $paginator = new Paginator($presentation);
 $paginator->render();
